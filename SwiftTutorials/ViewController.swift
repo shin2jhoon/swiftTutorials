82d4258234fc602172b418b22d842a2cfbc8907b
JungHoon <shin2jhoon@gmail.com>
2017-08-08 17:22:54 +0900

    Set new event

--- SwiftTutorials/ViewController.swift
+++ SwiftTutorials/ViewController.swift
@@ -8,16 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var chooseBtn: UIButton!
+    @IBOutlet weak var statePicker: UIPickerView!
+    @IBOutlet var entireView: UIView!
+    @IBOutlet weak var segueBtn: UIButton!
+    
+    let states = ["Alaska", "Arkansas", "Alabama", "California", "Maine", "New York"]
+    
+    @IBAction func ClickListener(_ sender: Any) {
+        
+//        if statePicker.isHidden {
+//            statePicker.isHidden = false
+//        }
+//        
+//        else{
+//            statePicker.isHidden = true
+//        }
+        
+//    entireView.isHidden = true
+        
+        
+        
+    }
+    
+    @IBAction func SegueListener(_ sender: Any) {
+        self.performSegue(withIdentifier: "explictSegue", sender: self)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        
-        print("Hello World!?!!!?")
-
-        
+        statePicker.dataSource = self
+        statePicker.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,6 +49,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
 
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        chooseBtn.setTitle(states[row], for: UIControlState.normal)
+        statePicker.isHidden = true
+    }
 }
 
